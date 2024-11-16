4013d5d5c74c09a4c25f9f0c84fdb713cf06dac3
matias <matyyassgonzalez89@gmail.com>
2024-11-15 21:50:37 -0300

    i fixed some bugs

--- src/main/java/challenge_meli/magneto/service/mapper/IDnaMapper.java
+++ src/main/java/challenge_meli/magneto/service/mapper/IDnaMapper.java
@@ -9,9 +9,7 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface IDnaMapper {
 
-    IDnaMapper INSTANCE = Mappers.getMapper(IDnaMapper.class);
 
-    @Mapping(target = "id", ignore = true) // Ignorar el ID ya que será generado automáticamente
     Dna dnaDTOtoEntity(DnaDTO dnaSequenceDTO);
 
     DnaDTO entityToDNADTO(Dna dnaSequence);
