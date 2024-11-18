8c9363535271ae9b87bd7020c54481f4ebef138d
matias <matyyassgonzalez89@gmail.com>
2024-11-18 20:04:30 -0300

    Docker added

--- src/main/java/challenge_meli/magneto/dto/DnaDTO.java
+++ src/main/java/challenge_meli/magneto/dto/DnaDTO.java
@@ -2,11 +2,14 @@ package challenge_meli.magneto.dto;
 
 import challenge_meli.magneto.model.Dna;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class DnaDTO {
 
     private String[] dna;
+
     private boolean isMutant;
 
     public DnaDTO() {}
@@ -14,5 +17,21 @@ public class DnaDTO {
     public DnaDTO(Dna dna) {
         this.dna = dna.getDna();
         this.isMutant = dna.isMutant();
+    }
+
+    public String[] getDna() {
+        return dna;
+    }
+
+    public void setDna(String[] dna) {
+        this.dna = dna;
+    }
+
+    public boolean isMutant() {
+        return isMutant;
+    }
+
+    public void setMutant(boolean mutant) {
+        isMutant = mutant;
     }
 }
--- src/main/java/challenge_meli/magneto/model/Dna.java
+++ src/main/java/challenge_meli/magneto/model/Dna.java
@@ -27,4 +27,24 @@ public class Dna {
         this.dna = dna;
         this.isMutant = isMutant;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String[] getDna() {
+        return dna;
+    }
+
+    public void setDna(String[] dna) {
+        this.dna = dna;
+    }
+
+    public boolean isMutant() {
+        return isMutant;
+    }
+
+    public void setMutant(boolean mutant) {
+        isMutant = mutant;
+    }
 }
